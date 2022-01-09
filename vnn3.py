f1ef88507404f4ca096fea5aab17eb69a28a89ca
hoang1704 <lethehoang67@gmail.com>
2022-01-09 18:15:49 +0700

    first commit

--- vnn3.py
+++
@@ -1,6 +0,0 @@
-import os
-#in ra tất cả các tệp/thư mục của đường dẫn được truyền vào
-a = os.listdir("/home/hoang/Desktop/qa")
-for i in range(0,len(a)):
-    if a[i] == 'vn':
-        print(a[i])
